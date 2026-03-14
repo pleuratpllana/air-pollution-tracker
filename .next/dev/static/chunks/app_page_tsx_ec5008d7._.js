c544fda7b7f1d970baf959dc61713d6375a8b075
pleuratpllana <your-email@example.com>
2026-03-14 11:22:03 +0100

    Add .gitignore to ignore node_modules, .next, and env files

--- .next/dev/static/chunks/app_page_tsx_ec5008d7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_18af3acf._.js",
-  "static/chunks/app_c1e1bb94._.js"
-],
-    source: "dynamic"
-});
